11a0b8dbed049c162bbe20fb3516068682dd8fd5
heather bickish <hbickish@gmail.com>
2024-12-02 13:53:35 -0700

    added moon

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { AppState } from './AppState.js'
+
+
+</script>
+
+
+<template>
+<header>
+  <div>
+    <h1 class="text-center my-3">Moon Miner</h1>
+  </div>
+</header>
+<main class="container-fluid">
+  <section class="row">
+    <div class="col-12">
+      <div class="text-center mt-5">
+        <img src="https://em-content.zobj.net/source/facebook/92/new-moon-with-face_1f31a.png" alt="">
+      </div>
+    </div>
+  </section>
+
+</main>
+</template>
+
+
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+img{
+  height: 20rem;
+}
+
+</style>
